9638a601ec8b058d4e82f3ec39ec0d43488e1db9
shuaiwangvu <shuai.wang.vu@gmail.com>
2020-06-08 19:28:39 +0200

    obtain broader and narrower n-triple

--- broader_narrower.py
+++ broader_narrower.py
@@ -69,207 +69,46 @@ file_narrower = open('narrower.nt', 'w', newline='')
 file_broader = open('broader.nt', 'w', newline='')
 
 
+file_integrated =  open('integrated.nt', 'w', newline='')
+file_narrower = open('narrower.nt', 'w', newline='')
+file_broader = open('broader.nt', 'w', newline='')
+
+
 triples, cardinality = hdt.search_triples("", narrower, "")
 print ('There are ', cardinality, 'narrower properties')
-# file =  open('narrower.csv', 'w', newline='')
-# writer = csv.writer(file)
-# writer.writerow([ "Subject", "Object"])
 
 for (s, p, o) in triples:
-    # writer.writerow([s, o])
-    # writer_integrated.writerow([s, o])
-    if validators.uri(o):
-        o = URIRef(o)
+    if s[0] == '"":
+        s = s
     else:
-        o = Literal(o)
-
-    if validators.uri(s):
-        s = URIRef(s)
+        s = '<' + s + '>'
+    if o[0] == '"":
+        o = o
     else:
-        s = Literal(s)
-
-    narrowerGraph.add((s, narrowerRef, o))
-    integratedGraph.add((s, narrowerRef, o))
-
+        o = '<' + o + '>'
+    p = '<' + p + '>'
+    file_narrower.write(s +' '+  p+  ' ' + o + '.\n' )
+    file_integrated.write(s +' '+  p+  ' ' + o + '.\n' )
 
 # file.close()
-narrowerGraph.serialize(destination='narrower.nt', format='nt')
+# narrowerGraph.serialize(destination='narrower.nt', format='nt')
 #
-# triples, cardinality = hdt.search_triples("", broader, "")
-# print ('There are ', cardinality, 'broader properties')
-# # file =  open('broader.csv', 'w', newline='')
-# # writer = csv.writer(file)
-# # writer.writerow([ "Subject", "Object"])
-#
-# for (s, p, o) in triples:
-#     # writer.writerow([s, o])
-#     # writer_integrated.writerow([o, s])
-#     # broaderGraph.add((URIRef(s), broaderRef, URIRef(o)))
-#     # integratedGraph.add((URIRef(o), narrowerRef, URIRef(s)))
-#     if validators.url(o):
-#         o = URIRef(o)
-#     else:
-#         o = Literal(o)
-#
-#     if validators.url(s):
-#         s = URIRef(s)
-#     else:
-#         s = Literal(s)
-#
-#     broaderGraph.add((s, broaderRef, o))
-#     integratedGraph.add((o, broaderRef, s))
-#
-# # file.close()
-# broaderGraph.serialize(destination='broader.nt', format='nt')
-# integratedGraph.serialize(destination='integrated.nt', format='nt')
+triples, cardinality = hdt.search_triples("", broader, "")
+print ('There are ', cardinality, 'broader properties')
+for (s, p, o) in triples:
+    if s[0] == '"":
+        s = s
+    else:
+        s = '<' + s + '>'
+    if o[0] == '"":
+        o = o
+    else:
+        o = '<' + o + '>'
+    p = '<' + p + '>'
+    file_broader.write(s +' '+  p+  ' ' + o + '.\n' )
+    file_integrated.write(o +' '+  p+  ' ' + s + '.\n' )
 
-#
-#
-# print ('\n\n')
-# print ('as subject:')
-# triples, cardinality = hdt.search_triples(narrower, "" ,"")
-# # print ('There are ', cardinality, 'narrower properties')
-# # collect_narrower_properties = set()
-# for (s, p, o) in triples:
-#     print ('\tpredicate:  ', p)
-#     print ('\tobject: ', o)
-#     print ('\n')
-#     # collect_narrower_properties.add(s)
-#
-# print ('\n\n')
-# print ('as object:')
-# triples, cardinality = hdt.search_triples("" ,"", narrower)
-# # print ('There are ', cardinality, 'narrower properties')
-# # collect_narrower_properties = set()
-# for (s, p, o) in triples:
-#     print ('\tsubject: ', s)
-#     print ('\tpredicate:  ', p)
-#     print ('\n')
-#     # collect_narrower_properties.add(s)
-#
-# triples, cardinality = hdt.search_triples("", "", s)
-# print ('There are ', cardinality, ' symmetric properties')
-# collect_symmetric_properties = set()
-# for (s, p, o) in triples:
-#     # print ('\tsubject: ', s)
-#     # print ('\tpredicate:  ', p)
-#     # print ('\n')
-#     collect_symmetric_properties.add(s)
-#
-# triples, cardinality = hdt.search_triples("", "", aS)
-# print ('There are ', cardinality, ' assymmetric properties')
-# collect_assymmetric_properties = set()
-# for (s, p, o) in triples:
-#     # print ('\tsubject: ', s)
-#     # print ('\tpredicate:  ', p)
-#     # print ('\n')
-#     collect_assymmetric_properties.add(s)
-#
-#
-#
-# triples, cardinality = hdt.search_triples("", "", r)
-# print ('There are ', cardinality, ' reflexive properties')
-# collect_reflexive_properties = set()
-# for (s, p, o) in triples:
-#     # print ('\tsubject: ', s)
-#     # print ('\tpredicate:  ', p)
-#     # print ('\n')
-#     collect_reflexive_properties.add(s)
-#
-#
-# triples, cardinality = hdt.search_triples("", "", iR)
-# print ('There are ', cardinality, ' irreflexive properties')
-# collect_irreflexive_properties = set()
-# for (s, p, o) in triples:
-#     # print ('\tsubject: ', s)
-#     # print ('\tpredicate:  ', p)
-#     # print ('\n')
-#     collect_irreflexive_properties.add(s)
-#
-#
-#
-# collect_equivalent_properties = collect_reflexive_properties.intersection(collect_symmetric_properties.intersection(collect_transitive_properties))
-# print ('There are ', len(collect_equivalent_properties), ' equivalent properties')
-#
-#
-# # ====
-# print ('not symmetric but reflexive:')
-# tmp = collect_transitive_properties.intersection(collect_reflexive_properties).difference(collect_assymmetric_properties).difference(collect_symmetric_properties)
-# print ('size: ', len(tmp))
-# if len(tmp) > 0:
-#     print ('example:', list(tmp)[0])
-#
-#
-# print ('not symmetric and not reflexive:')
-# tmp = collect_transitive_properties.difference(collect_reflexive_properties).difference(collect_irreflexive_properties).difference(collect_assymmetric_properties).difference(collect_symmetric_properties)
-# print ('size: ', len(tmp))
-# if len(tmp) > 0:
-#     print ('example:', list(tmp)[0])
-#
-# print ('not symmetric and irreflexive:')
-# tmp = collect_transitive_properties.intersection(collect_irreflexive_properties).difference(collect_assymmetric_properties).difference(collect_symmetric_properties)
-# print ('size: ', len(tmp))
-# if len(tmp) > 0:
-#     print ('example:', list(tmp)[0])
-#
-#
-# # =====
-# print ('#######')
-#
-# print ('symmetric and not reflexive:')
-# tmp = collect_transitive_properties.intersection(collect_symmetric_properties).difference(collect_irreflexive_properties).difference(collect_reflexive_properties)
-# print ('size: ', len(tmp))
-# if len(tmp) > 0:
-#     print ('example:', list(tmp)[0])
-#
-# print ('symmetric and irreflexive:')
-# tmp = collect_transitive_properties.intersection(collect_symmetric_properties).intersection(collect_irreflexive_properties)
-# print ('size: ', len(tmp))
-# if len(tmp) > 0:
-#     print ('example:', list(tmp)[0])
-#
-# # =====
-# print ('#######')
-#
-# print ('assymmetric and reflexive:')
-# tmp = collect_transitive_properties.intersection(collect_assymmetric_properties).intersection(collect_reflexive_properties)
-# print ('size: ', len(tmp))
-# if len(tmp) > 0:
-#     print ('example:', list(tmp)[0])
-#
-# print ('assymmetric and not reflexive:')
-# tmp = collect_transitive_properties.intersection(collect_assymmetric_properties).difference(collect_irreflexive_properties).difference(collect_reflexive_properties)
-# print ('size: ', len(tmp))
-# if len(tmp) > 0:
-#     print ('example:', list(tmp)[0])
-#
-# print ('assymmetric and irreflexive:')
-# tmp = collect_transitive_properties.intersection(collect_assymmetric_properties).intersection(collect_irreflexive_properties)
-# print ('size: ', len(tmp))
-# if len(tmp) > 0:
-#     print ('example:', list(tmp)[0])
 
-#
-# for b in both:
-#     # print ('\n\n')
-#     # prepare a graph:
-#     graph = nx.DiGraph()
-#     triples, cardinality = hdt.search_triples("", b, "")
-#     if cardinality >= 100000:
-#         print ('too large to find all cycles: ', cardinality,' for ', b)
-#     else:
-#         collect_pairs = []
-#         for (s, _, o) in triples:
-#             collect_pairs.append((s, o))
-#         graph.add_edges_from(collect_pairs)
-#         c = []
-#         try:
-#             c = nx.find_cycle(graph)
-#         except :
-#             # print ('no cycle found for ', b)
-#             pass
-#         if len(c) > 0:
-#             print('found ', len(c), ' cycles for ', b)
-#             print (c)
-#             print ('graph (edge) size = ',cardinality)
-#             print('\n')
+file_broader.close()
+file_narrower.close()
+file_integrated.close()
