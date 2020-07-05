3e4e23fb0315cf91922dce48fb95f77d2103ea3d
shuaiwangvu <shuai.wang.vu@gmail.com>
2020-07-05 20:38:16 +0200

    statistics property

--- statistics_property.py
+++ statistics_property.py
@@ -23,27 +23,37 @@ from rdflib import Graph, Literal, RDF, URIRef
 # from rdflib.namespace import FOAF , XSD
 import validators
 
-
+# http://rhm.cdepot.net/xml/properSubClassOf
 
 propertyGraph = Graph()
 PATH_LOD = "/scratch/wbeek/data/LOD-a-lot/data.hdt"
 hdt = HDTDocument(PATH_LOD)
-
-property = "http://www.w3.org/2002/07/owl#ObjectProperty"
+property = 'http://www.w3.org/1999/02/22-rdf-syntax-ns#Property'
+objectProperty = "http://www.w3.org/2002/07/owl#ObjectProperty"
 subClassOf = "http://www.w3.org/2000/01/rdf-schema#subClassOf"
+
+print('=====Property ======')
 triples, cardinality = hdt.search_triples("", subClassOf, property)
 for (s, p, o) in triples:
     _, scardinality = hdt.search_triples("", "", s)
     print ('property: ', s, ' : ',scardinality)
 
 
-ptriples, pcardinality = hdt.search_triples(property, "", property)
+``
+print('=====Object property =====')
+triples, cardinality = hdt.search_triples("", subClassOf, objectProperty)
+for (s, p, o) in triples:
+    _, scardinality = hdt.search_triples("", "", s)
+    print ('property: ', s, ' : ',scardinality)
+
+
+ptriples, pcardinality = hdt.search_triples(objectProperty, "", objectProperty)
 if pcardinality == 1:
     print('it is subclass of itself')
 
 
 print ('-------<as subject>--------')
 
-triples, cardinality = hdt.search_triples(property, "", "")
+triples, cardinality = hdt.search_triples(objectProperty, "", "")
 for (s, p, o) in triples:
     print ('p, o', p, o)
